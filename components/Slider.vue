88dbcc571479ba047f50a5b918d49f0fe6ed2fff
Pavlo Goncharenko <pavelgoncharenko23@gmail.com>
2021-03-10 16:05:39 +0300

    42

--- components/Slider.vue
+++ components/Slider.vue
@@ -19,6 +19,13 @@ export default {
     return {
       slides: [
         {
+          title: 'Relax Pepe #042',
+          description: 'ZombiePepe',
+          img: '48.jpg',
+          link:
+            'https://rarible.com/token/0x79741245a85b6ef4d22cf928b87ee69ef6f872c6:62:0x83fa662610b204080bcdbf1345905d86744a7495',
+        },
+        {
           title: 'Relax Pepe #041',
           description: 'MrCookiesPepe',
           img: '45.jpg',
