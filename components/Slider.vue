ea439b1c1af99d26703650e358f8515ab4fb99c0
Pavlo Goncharenko <pavelgoncharenko23@gmail.com>
2021-03-17 11:32:45 +0300

    fix typo

--- components/Slider.vue
+++ components/Slider.vue
@@ -20,7 +20,7 @@ export default {
       slides: [
     {
           title: 'Relax Pepe #050',
-          description: 'ZombiePepe v.2.0 [Unique]]',
+          description: 'ZombiePepe v.2.0 [Unique]',
           img: '55.jpg',
           link:
             'https://rarible.com/token/0x79741245a85b6ef4d22cf928b87ee69ef6f872c6:75:0x83fa662610b204080bcdbf1345905d86744a7495',
